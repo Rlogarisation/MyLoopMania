01ffc4bb4e9ffa29432a6809aa687da7d62547d4
Sameer <sameer.mahmud1@hotmail.com>
2021-07-14 17:25:40 +1000

    fixed sword strat

--- src/unsw/loopmania/SwordStrategy.java
+++ src/unsw/loopmania/SwordStrategy.java
@@ -1,9 +1,11 @@
 package unsw.loopmania;
 
 public class SwordStrategy implements FightStrategy{
-
+    private final int damage = 6;
     public void attack(double initialDamage, Enemy enemy) {
-        // TODO Auto-generated method stub
+        double currentHp = enemy.getHp();
+        double totalDamage = initialDamage+(damage*2);
+        enemy.setHp(currentHp-totalDamage);
     }
     
 }
